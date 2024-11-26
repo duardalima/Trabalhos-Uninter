d16d6ff2df763905fb25617045fc5ee43cf0c886
Maria Eduarda Lima Gonzaga <121740200+duardalima@users.noreply.github.com>
2024-11-26 11:00:59 -0300

    Update Principal.java

--- Uninter/Principal.java
+++ Uninter/Principal.java
@@ -1,8 +1,8 @@
 package empresa;
 
-public class Principal{
-    //   array
-    public static void main(String[]args){
-        System.out.println("Hello Word");
+public class Principal {
+    // Array
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
